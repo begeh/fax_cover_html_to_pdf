7db6d35f3be183b245475056c4172bbd71c5c356
Bashir <b_egeh@hotmail.com>
2020-04-11 18:01:11 -0400

    fix indentation in App.js

--- src/App.js
+++ src/App.js
@@ -6,40 +6,39 @@ import { Preview, print } from 'react-html2pdf';
 function App() {
 
   return (
-    <div >
+    <div className= "App">
     <Preview id={'fax'} >
       <div className="fax-header">
         <h1>FAX</h1>
-        <div>
-			<blockquote>
-				<p><span >Date:</span> </p>
-				<p><span >Pages including cover sheet:</span></p>
-			</blockquote>
+				<div>
+					<blockquote>
+						<p><span >Date:</span> </p>
+						<p><span >Pages including cover sheet:</span></p>
+					</blockquote>
 
-			<hr />
-			<h2 >To</h2>
-			<blockquote>
-				<p><span >Name:</span> </p>
-				<p><span >Phone:</span> </p>
-				<p><span >Fax Number:</span> </p>
-			</blockquote>
+					<hr />
+					<h2 >To</h2>
+					<blockquote>
+						<p><span >Name:</span> </p>
+						<p><span >Phone:</span> </p>
+						<p><span >Fax Number:</span> </p>
+					</blockquote>
 
-			<hr />
-			<h2 >From</h2>
-			<blockquote>
-				<p><span >Name:</span> </p>
-				<p><span >Phone:</span> </p>
-				<p><span >Fax Number:</span> </p>
-			</blockquote>
+					<hr />
+					<h2 >From</h2>
+					<blockquote>
+						<p><span >Name:</span> </p>
+						<p><span >Phone:</span> </p>
+						<p><span >Fax Number:</span> </p>
+					</blockquote>
 
-			<hr />
-			<h2>Note</h2>
-      <hr/>
-      <p>
-      Bashir
-      </p>
-
-		</div>
+					<hr />
+					<h2>Note</h2>
+					<hr/>
+					<p>
+					This is the document note
+					</p>
+				</div>
       </div>
       </Preview>
       <button onClick={()=>print('download_pdf', 'fax')}> print</button>
